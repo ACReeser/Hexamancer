3a6a4c058e41f8aa0f3788b4f5d64afa56de6a97
ACReeser <rogueconformist@gmail.com>
2018-02-03 12:29:12 -0600

    Main menu but not save/load

---
+++ Assets/Menu.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ViewState { LoggedOut, LoggingIn, LoggedIn, SelectLoadedMap, SetUpNewMap, Closed }
+
+public class Menu : MonoBehaviour {
+    public RectTransform modal, title, loggedInPanel, loggedOutPanel, loginPanel, loadMapPanel, newMapPanel;
+    public Input user, pass;
+
+    private ViewState CurrentViewState = ViewState.LoggedOut;
+
+    // Use this for initialization
+    void Start () {
+        modal.gameObject.SetActive(true);
+        RefreshViewState();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Toggle()
+    {
+        if (CurrentViewState == ViewState.Closed)
+        {
+            RefreshViewState(ViewState.LoggedIn);
+        }
+        else
+        {
+            RefreshViewState(ViewState.Closed);
+        }
+    }
+
+    public void OpenLoginInputs()
+    {
+        RefreshViewState(ViewState.LoggingIn);
+    }
+
+    private void RefreshViewState(ViewState? newViewState = null)
+    {
+        if (newViewState.HasValue)
+            CurrentViewState = newViewState.Value;
+
+        //switch (CurrentViewState)
+        //{
+        //    case 
+        //}
+        this.modal.gameObject.SetActive(CurrentViewState != ViewState.Closed);
+        this.title.gameObject.SetActive(CurrentViewState != ViewState.SelectLoadedMap);
+        this.loggedOutPanel.gameObject.SetActive(CurrentViewState == ViewState.LoggedOut);
+        this.loginPanel.gameObject.SetActive(CurrentViewState == ViewState.LoggingIn);
+        this.loggedInPanel.gameObject.SetActive(CurrentViewState == ViewState.LoggedIn);
+        this.loadMapPanel.gameObject.SetActive(CurrentViewState == ViewState.SelectLoadedMap);
+        //this.newMapPanel.gameObject.SetActive(CurrentViewState == ViewState.SetUpNewMap);
+    }
+
+    public void Login()
+    {
+        RefreshViewState(ViewState.LoggedIn);
+    }
+    public void BackToLoggedIn()
+    {
+        RefreshViewState(ViewState.LoggedIn);
+    }
+    public void LogOut()
+    {
+        RefreshViewState(ViewState.LoggedOut);
+    }
+    public void SetupNewMap()
+    {
+        //RefreshViewState(ViewState.SetUpNewMap);
+        RefreshViewState(ViewState.Closed);
+    }
+    public void OpenSavedMaps()
+    {
+        RefreshViewState(ViewState.SelectLoadedMap);
+    }
+
+
+}
---
+++ Assets/TitleHexAnimate.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleHexAnimate : MonoBehaviour {
+    public RectTransform[] titleCharacters;
+
+    private const float duration = .5f;
+    private float time = 0f;
+    private const float holdDuration = 10f;
+    public bool pulsing = false;
+    
+	
+	void OnEnable() {
+        foreach(var t in titleCharacters)
+            t.localScale = Vector3.zero;
+
+        pulsing = true;
+        StartCoroutine(Pulse());
+	}
+
+    private IEnumerator Pulse()
+    {
+        float time = 0f;
+        while (time < 2f)
+        {
+            for (int i = 0; i < titleCharacters.Length; i++)
+            {
+                float delay = i * .1f;
+                if (i > 2)
+                    delay += .2f;
+                if (i > 3)
+                    delay += .2f;
+
+                titleCharacters[i].localScale = Vector3.Lerp(Vector3.zero, Vector3.one, (time - delay) / .5f);
+            }
+
+            yield return null;
+            time += Time.deltaTime;
+        }
+        yield return new WaitForSeconds(10f);
+        time = 0f;
+        while (time < 2f)
+        {
+            for (int i = 0; i < titleCharacters.Length; i++)
+            {
+                float delay = i * .1f;
+                if (i > 2)
+                    delay += .2f;
+                if (i > 3)
+                    delay += .2f;
+
+                titleCharacters[i].localScale = Vector3.Lerp(Vector3.one, Vector3.zero, (time - delay) / .5f);
+            }
+
+            yield return null;
+            time += Time.deltaTime;
+        }
+        pulsing = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (!pulsing)
+        {
+            pulsing = true;
+            StartCoroutine(Pulse());
+        }
+	}
+}

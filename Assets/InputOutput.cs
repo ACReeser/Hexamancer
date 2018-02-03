d4f6377ebace00d14e59684214e23b5b88a69a7a
ACReeser <rogueconformist@gmail.com>
2018-02-02 22:05:04 -0600

    Fix for desktop IO

--- Assets/InputOutput.cs
+++ Assets/InputOutput.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.Networking;
 
 [Serializable]
 public class Library
@@ -114,13 +115,99 @@ public class InputOutput : MonoBehaviour {
         public Library[] LoadedLibraries { get; private set; }
         public IEnumerator GetLibraries()
         {
-            bool isWeb = Application.dataPath.StartsWith("http");
-            string basePath = Application.dataPath;
-            yield return null;
+            //string basePath = Application.dataPath;
+            var www = new UnityWebRequest(Application.dataPath + "/Resources/Libraries/");
+            www.SetRequestHeader("Accept", "application/json");
+            www.downloadHandler = new DownloadHandlerBuffer();
+            yield return www.Send();
+
+            if (www.isError)
+            {
+                Debug.Log(www.error);
+            }
+            else {
+                Debug.Log(www.downloadHandler.text);
+                var files = JsonHelper.getJsonArray<CaddyFileInfo>(www.downloadHandler.text);
+                // Or retrieve results as binary data
+                //byte[] results = www.downloadHandler.data;
+
+                var libList = new List<Library>();
+                foreach(var file in files)
+                {
+                    if (file.IsDir)
+                    {
+                        libList.Add(new Library()
+                        {
+                            Name = file.Name
+                        });
+                    }
+                }
+                LoadedLibraries = libList.ToArray();
+            }
         }
         public IEnumerator LoadIcons(Library lib)
         {
-            yield return null;
+            var libraryListRequest = new UnityWebRequest(Application.dataPath + "/Resources/Libraries/"+lib.Name+"/Icons/");
+            libraryListRequest.SetRequestHeader("Accept", "application/json");
+            libraryListRequest.downloadHandler = new DownloadHandlerBuffer();
+            yield return libraryListRequest.Send();
+
+            if (libraryListRequest.isError)
+            {
+                Debug.Log(libraryListRequest.error);
+            }
+            else {
+                Debug.Log(libraryListRequest.downloadHandler.text);
+                var files = JsonHelper.getJsonArray<CaddyFileInfo>(libraryListRequest.downloadHandler.text);
+
+                var sprites = new List<Sprite>();
+                foreach (var file in files)
+                {
+                    if (!file.IsDir && !file.Name.EndsWith("meta"))
+                    {
+                        var spriteRequest = new UnityWebRequest(Application.dataPath + "/Resources/Libraries/" + lib.Name + "/Icons/"+file.Name);
+                        spriteRequest.SetRequestHeader("Accept", "application/json");
+                        spriteRequest.downloadHandler = new DownloadHandlerTexture();
+                        yield return spriteRequest.Send();
+
+                        if (spriteRequest.isError)
+                        {
+                            Debug.Log(libraryListRequest.error);
+                        }
+                        else
+                        {
+                            var tex = (spriteRequest.downloadHandler as DownloadHandlerTexture).texture;
+                            var newSprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(.5f, .5f));
+                            newSprite.name = Path.GetFileNameWithoutExtension(file.Name);
+                            sprites.Add(newSprite);
+                        }
+                    }
+                }
+                lib.Icons = sprites.ToArray();
+            }
+        }
+    }
+
+    [Serializable]
+    public class CaddyFileInfo
+    {
+        public bool IsDir;
+        public string Name;
+    }
+
+    public class JsonHelper
+    {
+        public static T[] getJsonArray<T>(string json)
+        {
+            string newJson = "{ \"array\": " + json + "}";
+            Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(newJson);
+            return wrapper.array;
+        }
+
+        [Serializable]
+        private class Wrapper<T>
+        {
+            public T[] array;
         }
     }
 }
